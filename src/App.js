5c7e139d71c1a17f30f742400594e434fcdf4d06
marwane <marwane@gmail.com>
2024-02-08 00:23:46 +0100

    To do list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useRef, useState } from 'react';
 function App() {
+  var [divs, setdivs] = useState([]);
+  const valueRef = useRef(null);
+
+  const Add = (task) => {
+    console.log(task)
+    const nouvelleDiv = <div className='container' key={divs.length}>
+      <h3 className=''>Task:</h3>
+      <h4>{task}</h4>
+    </div>;
+    setdivs([...divs, nouvelleDiv])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Appl">
+      <h1 className='title'>My to do list App</h1>
+      <div class="row">
+        <div class="col">
+          <input ref={valueRef} type="text" class="styled-input" placeholder="First name" aria-label="First name"></input>
+          <button onClick={() => { Add(valueRef.current.value) }} className='styled-button'>Add</button>
+        </div>
+      </div>
+      {divs.map((div, index) => (
+        <div key={index}>{div}</div>
+      ))}
+
     </div>
   );
 }
+
 
 export default App;
